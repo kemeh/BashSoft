a936d1b4368af57b41d523cbf0a213b353ddea6c
kemeh <knm1988@gmail.com>
2017-06-03 14:12:40 +0300

    Project Started

---
+++ BashSoft/OutputWriter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    public static class OutputWriter
+    {
+        public static void WriteMessage(string message)
+        {
+            
+        }
+
+        public static void WriteMessageOnNewLine(string message)
+        {
+
+        }
+
+        public static void WriteEmptyLine()
+        {
+
+        }
+
+        public static void DisplayException(string message)
+        {
+
+        }
+    }
+}
